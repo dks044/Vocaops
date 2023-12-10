5fad11d55414575c9e87a27e5b28da66e1dfd9d4
AJN <buzz781@gmail.com>
2023-12-10 15:40:54 +0900

    feat: User 관련 엔티티,enum 구현

--- src/main/java/com/vocaops/user/SiteUser.java
+++ src/main/java/com/vocaops/user/SiteUser.java
@@ -1,5 +1,32 @@
 package com.vocaops.user;
 
-public class SiteUser {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class SiteUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String password;
+	
+	private String name;
+	
+	private String phoneNumber;
+	
+	private String email;
+	
+    @Column(unique = true)
+    private String phone_number; //폰번호
+    
+    @Enumerated(EnumType.STRING)
+    private UserRole role; //권한
+    
+    private long rating;
 }
