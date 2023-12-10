1a85c1553ede87013b5710e428852e56d38a5fc3
AJN <buzz781@gmail.com>
2023-12-10 15:15:44 +0900

    Initial commit

---
+++ src/main/java/com/wordwave/main/VocaopsApplication.java
@@ -0,0 +1,13 @@
+package com.wordwave.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaopsApplication.class, args);
+	}
+
+}
