0b2e7d6781f10d8c0454bf6b86c2cbe0b63a8573
AJN <buzz781@gmail.com>
2023-12-10 20:42:22 +0900

    test: 구글번역기API 테스트 및 API키 환경변수 설정

--- src/test/java/com/vocaops/main/GoogleTranslateAPITest.java
+++ src/test/java/com/vocaops/main/GoogleTranslateAPITest.java
@@ -3,7 +3,6 @@ package com.vocaops.main;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
@@ -14,8 +13,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class GoogleTranslateAPITest {
 
-    @Value("${google.cloud.translation.api-key}")
-    private String apiKey;
+    
+	 @Value("${google.cloud.translation.api-key}")
+	  private String apiKey;
 
     @DisplayName("구글번역기 API 테스트")
     @Test
@@ -27,8 +27,6 @@ public class GoogleTranslateAPITest {
 
         System.out.printf("Translated Text: %s%n", translation.getTranslatedText());
 
-        // 예상 결과와 실제 결과 비교
-        // 여기서는 "Hello"라는 예상 결과를 사용합니다만, 실제 번역 결과는 API의 내부 로직에 따라 다를 수 있으므로 적절히 조정해야 합니다.
-        assertEquals("Hello", translation.getTranslatedText());
+         assertEquals("hello", translation.getTranslatedText());
     }
 }
